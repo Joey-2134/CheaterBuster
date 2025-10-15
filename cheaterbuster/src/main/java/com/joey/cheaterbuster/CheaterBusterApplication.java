d10ebb88f999e4ebe88f699cf02e2eb2c85beeaf
joeyg <joeygalvin2134@gmail.com>
2025-10-15 11:27:45 +0100

    init

---
+++ cheaterbuster/src/main/java/com/joey/cheaterbuster/CheaterBusterApplication.java
@@ -0,0 +1,13 @@
+package com.joey.cheaterbuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheaterBusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheaterBusterApplication.class, args);
+	}
+
+}
---
+++ cheaterbuster/src/test/java/com/joey/cheaterbuster/CheaterBusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.cheaterbuster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheaterBusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

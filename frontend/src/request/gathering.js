d83c54c36e047f3b824e958cbee8e515195f6c08
Joey Galvin <joeygalvin2134@gmail.com>
2025-10-27 11:54:23 +0000

    setting up count

---
+++ frontend/src/request/gathering.js
@@ -0,0 +1,7 @@
+import {API_BASE_URL} from "../util/constants.js";
+
+export const getPlayerCount = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/gathering/count`);
+    if (!response.ok) throw new Error(`API error: ${response.status}`);
+    return await response.json();
+};

e2351be3b31459d4d97002428131803068964a78
Joey Galvin <joeygalvin2134@gmail.com>
2025-10-27 12:19:21 +0000

    added api key

--- frontend/src/request/gathering.js
+++ frontend/src/request/gathering.js
@@ -1,7 +1,12 @@
 import {API_BASE_URL} from "../util/constants.js";
 
 export const getPlayerCount = async () => {
-    const response = await fetch(`${API_BASE_URL}/api/gathering/count`);
+    const response = await fetch(`${API_BASE_URL}/api/gathering/count`, {
+        headers: {
+            'X-API-Key': import.meta.env.VITE_API_KEY
+        }
+    });
     if (!response.ok) throw new Error(`API error: ${response.status}`);
     return await response.json();
 };
+
--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://18.204.11.229:8080/api';
+export const API_BASE_URL = 'http://44.220.56.109:8080/api';
